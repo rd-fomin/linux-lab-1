e29253ffaa9eb935962c9a011024547863708bd8
Roman <46630342+rd-fomin@users.noreply.github.com>
2020-12-24 14:51:12 +0300

    Create 01laba.sh

---
+++ 01laba.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+exec 0<>/dev/console 1<>/dev/console 2<>/dev/console
+cat <<'msgend'
+<Roman's dracut module>
+Roman Fomin, M3O-117M-20
+[Roman] (c) 2020
+ |^^^^^^^^^^^^^^||_____
+ |The STFU Truck||__|__\__,_
+ |______________||__|__|__|)
+...(@)(@)-------* (@)(@)*|(@)
+
+[Roman]
+msgend
+sleep 5
+echo " continuing...."
